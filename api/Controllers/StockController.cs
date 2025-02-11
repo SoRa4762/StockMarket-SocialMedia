af4fd0fb4b3df613eb42cd248f5f7562d28ac518
SoRa <sonamraisx007@gmail.com>
2025-02-11 19:32:55 +0545

    CRUD w Async

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using api.Data;
+using api.Dtos.Stock;
 using api.Mappers;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -22,22 +23,69 @@ namespace api.Controllers
         }
 
         [HttpGet]
-        public IActionResult GetAll() //IActionResult is a wrapper function that saves you from writing whole bunch of code just to return 500 ok message with some data
+        public async Task<IActionResult> GetAll() //IActionResult is a wrapper function that saves you from writing whole bunch of code just to return 500 ok message with some data
         {
-            var stocks = _context.Stocks.ToList().Select(s => s.ToStockDto()); //deferred execution
+            var stocks = await _context.Stocks.ToListAsync();
+            var stockDto = stocks.Select(s => s.ToStockDto()); //deferred execution
             return Ok(stocks);
         }
 
         [HttpGet("{id}")]
-        public IActionResult GetById([FromRoute] int id) //dotnet uses model binding to extract that string and turn it into int and make sure we use it in the code
+        public async Task<IActionResult> GetById([FromRoute] int id) //dotnet uses model binding to extract that string and turn it into int and make sure we use it in the code
         {
-            var stock = _context.Stocks.Find(id);
+            var stock = await _context.Stocks.FindAsync(id);
             if (stock == null)
             {
                 return NotFound();
             }
             return Ok(stock.ToStockDto());
 
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateStockRequestDto stockDto)
+        {
+            var stockModel = stockDto.ToStockFromCreateDTO();
+            await _context.Stocks.AddAsync(stockModel);
+            await _context.SaveChangesAsync();
+            // get the new data and return it, logic is simple, syntax is not
+            return CreatedAtAction(nameof(GetById), new { id = stockModel.Id }, stockModel.ToStockDto());
+        }
+
+        //put is very intuitive
+        [HttpPut]
+        [Route("{id}")]
+        public async Task<IActionResult> Update([FromRoute] int id, [FromBody] UpdateStockRequestDto updateDto)
+        {
+            var stockModel = await _context.Stocks.FirstOrDefaultAsync(x => x.Id == id);
+            if (stockModel == null)
+            {
+                return NotFound();
+            }
+
+            stockModel.Symbol = updateDto.Symbol;
+            stockModel.CompanyName = updateDto.CompanyName;
+            stockModel.Purchase = updateDto.Purchase;
+            stockModel.LastDiv = updateDto.LastDiv;
+            stockModel.Industry = updateDto.Industry;
+            stockModel.MarketCap = updateDto.MarketCap;
+            await _context.SaveChangesAsync();
+            return Ok(stockModel.ToStockDto());
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<IActionResult> Delete([FromRoute] int id)
+        {
+            var stockModel = await _context.Stocks.FirstOrDefaultAsync(x => x.Id == id);
+            if (stockModel == null)
+            {
+                return NotFound();
+            }
+
+            _context.Stocks.Remove(stockModel);
+            await _context.SaveChangesAsync();
+            return NoContent();
         }
     }
 
--- api/Mappers/StockMapper.cs
+++ api/Mappers/StockMapper.cs
@@ -11,6 +11,9 @@ namespace api.Mappers
 {
     public static class StockMappers //static because they are going to be extention methods
     {
+        //here I have passed Stock - Model as parameters, because I want to manipulate data of the model
+        //here StockDto is the return type of the ToStockDto method
+        //turns Model to DTO
         public static StockDto ToStockDto(this Stock stockModel)
         {
             return new StockDto
@@ -22,6 +25,22 @@ namespace api.Mappers
                 LastDiv = stockModel.LastDiv,
                 Industry = stockModel.Industry,
                 MarketCap = stockModel.MarketCap
+            };
+        }
+
+        //here I have passed CreateStockRequestDto - DTO as parameters, because I want the data to be manipulated through the DTO
+        //here Stock is the return type of method, it turns CreateStockRequestDto to Stock
+        //turns DTO to model
+        public static Stock ToStockFromCreateDTO(this CreateStockRequestDto stockDto)
+        {
+            return new Stock
+            {
+                Symbol = stockDto.Symbol,
+                CompanyName = stockDto.CompanyName,
+                Purchase = stockDto.Purchase,
+                LastDiv = stockDto.LastDiv,
+                Industry = stockDto.Industry,
+                MarketCap = stockDto.MarketCap
             };
         }
     }
