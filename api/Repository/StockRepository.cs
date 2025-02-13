9108c64a8eadeb9e3f89ba48898b8e0de9a67071
SoRa <sonamraisx007@gmail.com>
2025-02-13 21:24:13 +0545

    Refactor to Repository

--- api/Repository/StockRepository.cs
+++ api/Repository/StockRepository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.Data;
+using api.Dtos.Stock;
 using api.Interfaces;
 using api.Models;
 using Microsoft.EntityFrameworkCore;
@@ -17,9 +18,32 @@ namespace api.Repository
         {
             _context = context;
         }
+
+        public async Task<Stock> CreateAsync(Stock stockModel)
+        {
+            await _context.Stocks.AddAsync(stockModel);
+            await _context.SaveChangesAsync();
+            return stockModel;
+        }
+
+        public Task<Stock?> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
         public Task<List<Stock>> GetAllAsync()
         {
             return _context.Stocks.ToListAsync();
+        }
+
+        public Task<Stock?> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Stock?> UpdateAsync(int id, UpdateStockRequestDto stockDto)
+        {
+            throw new NotImplementedException();
         }
     }
 }
